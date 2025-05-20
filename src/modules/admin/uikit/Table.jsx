b4254cf98a3e23ffebac751c6da9187ad96afa2c
Ezzat <khaqanezzat007@gmail.com>
2025-05-20 23:16:41 +0430

    feat: enhance DataTable component with dynamic data and columns; update styles for improved layout

--- src/modules/admin/uikit/Table.jsx
+++ src/modules/admin/uikit/Table.jsx
@@ -2,11 +2,178 @@ import { DataTable } from '../../../shared';
 import { PageHeader } from '../../../shared/index';
 
 export const Table = () => {
+  const columns = [
+    {
+      label: 'header 1',
+    },
+    {
+      label: 'header 2',
+    },
+    {
+      label: 'header 3',
+    },
+    {
+      label: 'header 4',
+    },
+    {
+      label: 'header 5',
+    },
+    {
+      label: 'header 6',
+    },
+  ];
+  const data = [
+    {
+      id: 1,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 2,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 3,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 4,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 5,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 6,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 7,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 8,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 9,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 10,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 11,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 12,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 13,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 14,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 15,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+    {
+      id: 16,
+      data1: 'data 1',
+      data2: 'data 2',
+      data3: 'data 3',
+      data4: 'data 4',
+      data5: 'data 5',
+      data6: 'data 6',
+    },
+  ];
+
   return (
     <>
       <PageHeader title="Data Table" />
 
-      <DataTable />
+      <DataTable columns={columns} data={data} />
     </>
   );
 };
--- src/shared/components/Table/DataTable.jsx
+++ src/shared/components/Table/DataTable.jsx
@@ -1,10 +1,34 @@
 import './styles/table.css';
 
-export const DataTable = () => {
+export const DataTable = ({ columns = [], data = [] }) => {
   return (
     <>
-      <div className="datatable-wrapper">
-        <h1>Table</h1>
+      <div className="datatable-table">
+        <div className="datatable-table-content">
+          <table>
+            <thead>
+              <tr>
+                <th>No</th>
+                {columns.map((colum, index) => (
+                  <th key={index}>{colum.label}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((row, index) => (
+                <tr key={row.id}>
+                  <td>{index + 1}</td>
+                  <td>{row.data1}</td>
+                  <td>{row.data2}</td>
+                  <td>{row.data3}</td>
+                  <td>{row.data4}</td>
+                  <td>{row.data5}</td>
+                  <td>{row.data6}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
