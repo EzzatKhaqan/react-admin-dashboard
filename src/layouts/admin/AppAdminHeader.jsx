01baed67deea1146e8aec2a1daf3bdd76c884e44
Ezzat <khaqanezzat007@gmail.com>
2025-05-22 06:29:42 +0430

    Refactor code structure for improved readability and maintainability,add styles to topbar

--- src/layouts/admin/AppAdminHeader.jsx
+++ src/layouts/admin/AppAdminHeader.jsx
@@ -1,8 +1,29 @@
+import img from '../../assets/images/logo/IMG_5567.jpg';
+
 export const AppAdminHeader = () => {
   return (
     <>
       <div className="nav">
-        <h1>Header</h1>
+        <div className="nav-logo">
+          <div className="logo">
+            <img src={img} alt="" />
+          </div>
+          <div className="nav-title">
+            <span>EzZat</span> React
+          </div>
+        </div>
+        <div className="nav-options">
+          <div className="nav-theme">
+            <div className="toggle-theme-btn">
+              <i className="pi pi-moon"></i>
+            </div>
+          </div>
+          <div className="nav-user">
+            <div className="user">
+              <i className="pi pi-user"></i>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
