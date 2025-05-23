eb7027fc2ebdbedab6ed5f2e023b659696d428a8
Ezzat <khaqanezzat007@gmail.com>
2025-05-23 10:46:00 +0430

    fix: update MenuItem component to use useLocation for active route detection; remove unused state

--- src/shared/components/menu/MenuItem.jsx
+++ src/shared/components/menu/MenuItem.jsx
@@ -2,20 +2,26 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { AnimatePresence, motion } from 'motion/react';
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
 export const MenuItem = ({ item = [], root = true }) => {
   const hasItem = Array.isArray(item.items) && item.items.length > 0;
   const isLink = Boolean(item.to);
   const Wrapper = isLink ? Link : 'a';
   const wrapperProps = isLink ? { to: item.to } : {};
+  const location = useLocation();
 
-  const [isActiveMenu, setIsActiveMenu] = useState(false);
+  function checkActiveRoute(item) {
+    if (item.to) return item.to == location.pathname;
+    else return false;
+  }
 
   return (
     <>
-      <li className={[root && 'root-menuitem', isActiveMenu && 'active-menuitem'].join(' ')}>
+      <li className={[root ? 'root-menuitem' : ''].join(' ')}>
         {root && <div className="menuitem-root-text">{item.label}</div>}
         {!root && (
-          <Wrapper {...wrapperProps}>
+          <Wrapper {...wrapperProps} className={[checkActiveRoute(item) ? 'active-route' : '']}>
             <i className={[item.icon, 'menuitem-icon'].join(' ')}></i>
             <span className="menuitem-text">{item.label}</span>
             {hasItem && <i className="pi pi-angle-down submenu-toggler"></i>}
