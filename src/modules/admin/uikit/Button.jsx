e84dfaf97e2b8fe537c4fb2eb7a20b4bcb6364ad
Ezzat <khaqanezzat007@gmail.com>
2025-05-26 22:09:35 +0430

    feat: enhance button styles(funtion and mixins[more powerfull]) and add disabled states for all button types

--- src/modules/admin/uikit/Button.jsx
+++ src/modules/admin/uikit/Button.jsx
@@ -10,27 +10,26 @@ export const Button = () => {
             <div className="flex flex-wrap gap-2">
               <AppButton label="Button" />
               <AppButton label="Disabled" disabled={true} />
-              <AppButton label="Primary" type="primary" />
             </div>
           </div>
           <div className="card flex flex-col gap-2">
             <h1>Types</h1>
             <div className="flex flex-wrap gap-2">
-              <AppButton label="Disabled" type="primary" disabled={true} />
               <AppButton label="Primary" type="primary" />
               <AppButton label="Secondary" type="secondary" />
-              <AppButton label="Disabled" type="secondary" disabled={true} />
               <AppButton label="Success" type="success" />
-              <AppButton label="Disabled" type="success" disabled={true} />
               <AppButton label="Info" type="info" />
-              <AppButton label="Disabled" type="info" disabled={true} />
               <AppButton label="Warn" type="warn" />
-              <AppButton label="Disabled" type="warn" disabled={true} />
               <AppButton label="Help" type="help" />
-              <AppButton label="Disabled" type="help" disabled={true} />
               <AppButton label="Danger" type="danger" />
-              <AppButton label="Disabled" type="danger" disabled={true} />
               <AppButton label="Contrast" type="contrast" />
+              <AppButton label="Disabled" type="primary" disabled={true} />
+              <AppButton label="Disabled" type="secondary" disabled={true} />
+              <AppButton label="Disabled" type="success" disabled={true} />
+              <AppButton label="Disabled" type="info" disabled={true} />
+              <AppButton label="Disabled" type="warn" disabled={true} />
+              <AppButton label="Disabled" type="help" disabled={true} />
+              <AppButton label="Disabled" type="danger" disabled={true} />
               <AppButton label="Disabled" type="contrast" disabled={true} />
             </div>
           </div>
@@ -39,6 +38,7 @@ export const Button = () => {
           <div className="card flex flex-col gap-2">
             <h1>Outlined</h1>
             <div className="flex flex-wrap gap-2">
+              <AppButton label="Primary" type="primary" outlined={true} />
               <AppButton label="Secondary" type="secondary" outlined={true} />
               <AppButton label="Success" type="success" outlined={true} />
               <AppButton label="Info" type="info" outlined={true} />
@@ -46,6 +46,14 @@ export const Button = () => {
               <AppButton label="Help" type="help" outlined={true} />
               <AppButton label="Danger" type="danger" outlined={true} />
               <AppButton label="Contrast" type="contrast" outlined={true} />
+              <AppButton label="Primary" type="primary" outlined={true} disabled={true} />
+              <AppButton label="Secondary" type="secondary" outlined={true} disabled={true} />
+              <AppButton label="Success" type="success" outlined={true} disabled={true} />
+              <AppButton label="Info" type="info" outlined={true} disabled={true} />
+              <AppButton label="Warn" type="warn" outlined={true} disabled={true} />
+              <AppButton label="Help" type="help" outlined={true} disabled={true} />
+              <AppButton label="Danger" type="danger" outlined={true} disabled={true} />
+              <AppButton label="Contrast" type="contrast" outlined={true} disabled={true} />
             </div>
           </div>
           <div className="card flex flex-col gap-2">
@@ -58,6 +66,13 @@ export const Button = () => {
               <AppButton label="Help" type="help" rounded={true} />
               <AppButton label="Danger" type="danger" rounded={true} />
               <AppButton label="Contrast" type="contrast" rounded={true} />
+              <AppButton label="Secondary" type="secondary" rounded={true} disabled={true} />
+              <AppButton label="Success" type="success" rounded={true} disabled={true} />
+              <AppButton label="Info" type="info" rounded={true} disabled={true} />
+              <AppButton label="Warn" type="warn" rounded={true} disabled={true} />
+              <AppButton label="Help" type="help" rounded={true} disabled={true} />
+              <AppButton label="Danger" type="danger" rounded={true} disabled={true} />
+              <AppButton label="Contrast" type="contrast" rounded={true} disabled={true} />
             </div>
           </div>
         </div>
